1c2568b7cb9133c4b9011712bd7247bcff145145
guojia99 <guojia99@foxmail.com>
2023-06-21 00:08:15 +0800

    添加api

--- api/route.go
+++ api/route.go
@@ -5,3 +5,42 @@
  */
 
 package api
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func (c *Client) AddRoute() {
+	route := c.e.Group("/")
+
+	api := route.Group("/api")
+	{
+		curd(api.Group("/players"), nil, nil, nil, nil, nil)  // 选手增删改查
+		curd(api.Group("/contests"), nil, nil, nil, nil, nil) // 比赛增删改查
+
+		// 成绩
+		score := api.Group("/score")
+		{
+			score.POST("/user/:user_id/contest/:contest_id")   // 上传成绩
+			score.DELETE("/user/:user_id/contest/:contest_id") // 移除成绩
+
+			report := score.GET("/report")
+			report.GET("/user/:user_id")        // 个人成绩统计
+			report.GET("/contests/:contest_id") // 某比赛成绩统计
+			report.GET("/best")
+		}
+	}
+
+	web := route.Group("/web")
+	{
+		web.GET("/")
+	}
+}
+
+func curd(route *gin.RouterGroup, c, r, rMui, u, d gin.HandlerFunc) {
+	route.GET("/:id", r)
+	route.GET("/", rMui)
+	route.POST("/", c)
+	route.PUT("/:id", u)
+	route.DELETE("/", d)
+}
