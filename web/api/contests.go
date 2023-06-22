45057ae89c45f9b207a25ebbbc84907837348483
guojia99 <guojia99@foxmail.com>
2023-06-22 23:00:08 +0800

    修改api实现

---
+++ web/api/contests.go
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2023 guojia99 All rights reserved.
+ * Created: 2023/6/22 下午6:28.
+ * Author:  guojia(https://github.com/guojia99)
+ */
+
+package api
+
+import "github.com/gin-gonic/gin"
+
+func CreateContest(ctx *gin.Context) {
+
+}
+
+func ReadContest(ctx *gin.Context) {
+
+}
+
+func ReadContests(ctx *gin.Context) {
+
+}
+
+func UpdateContest(ctx *gin.Context) {
+
+}
+
+func DeleteContest(ctx *gin.Context) {
+
+}
---
+++ web/api/score.go
@@ -0,0 +1,7 @@
+/*
+ * Copyright (c) 2023 guojia99 All rights reserved.
+ * Created: 2023/6/22 下午6:41.
+ * Author:  guojia(https://github.com/guojia99)
+ */
+
+package api
