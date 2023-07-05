18af5ce368ba18c84286a0109cde7c00d120ae24
guojia <guojia@guojia.com>
2023-07-05 15:39:33 +0800

    [修改] 修改数据库实现

--- db/db.go
+++ db/db.go
@@ -7,27 +7,40 @@
 package db
 
 import (
+	"os"
+
+	json "github.com/json-iterator/go"
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 var (
-	DB       *gorm.DB
-	dbDriver = "mysql"
+	DB *gorm.DB
 )
 
-func init() {
-	var err error
+type Config struct {
+	Db struct {
+		Driver string `json:"driver"`
+		Path   string `json:"path"`
+	} `json:"db"`
+}
 
-	switch dbDriver {
+func init() {
+	configBody, err := os.ReadFile("./config.json")
+	if err != nil {
+		panic(err)
+	}
+	var cfg Config
+	if err = json.Unmarshal(configBody, &cfg); err != nil {
+		panic(err)
+	}
+
+	switch cfg.Db.Driver {
 	case "sqlite":
-		DB, err = gorm.Open(sqlite.Open("./database.db"), &gorm.Config{})
+		DB, err = gorm.Open(sqlite.Open(cfg.Db.Path), &gorm.Config{})
 	case "mysql":
-		cfg := mysql.Config{
-			DSN: "root:my123456@tcp(127.0.0.1:3306)/mycube?charset=utf8&parseTime=True&loc=Local", // DSN data source name
-		}
-		DB, err = gorm.Open(mysql.New(cfg), &gorm.Config{})
+		DB, err = gorm.Open(mysql.New(mysql.Config{DSN: cfg.Db.Path}), &gorm.Config{})
 	}
 	if err != nil {
 		panic(err)
